0b18a371eaebedd7715d6f8611dfc2e30c02dc18
Smith <smithwebdd@gmail.com>
2019-08-27 10:30:20 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/create'
-  get 'sessions/destroy'
+  root 'home#index'
+  get "/auth/:provider/callback" => 'sessions#create'
+  get "/signout" => 'sessions#destroy', :as => :signout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
